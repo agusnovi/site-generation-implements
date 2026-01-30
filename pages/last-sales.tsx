6f2816128bf74d42bc4bac600e29d7f1ad8e5012
Agus Novianto <agus.n.jogja@gmail.com>
2026-01-30 17:25:02 +0700

    fix: combine ssg and csr

--- pages/last-sales.tsx
+++ pages/last-sales.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react"
+import useSWR from "swr"
 
 type User = {
     id: string;
@@ -9,11 +10,32 @@ type User = {
 type Response = {
     [key: string]: Omit<User, "id">
 }
-export default function LastSales() {
-    const [data, setData] = useState<User[]>([]);
-    const [isLoading, setLoading] = useState(false)
+
+function normalizeData(data: Response) {
+    const dataArr: User[] = []
+    for (const key in data) {
+        dataArr.push({
+            id: key,
+            username: data[key].username,
+            volume: data[key].volume
+        })
+    }
+    return dataArr;
+}
+
+export default function LastSales(props: { data: User[] }) {
+    const [sales, setSales] = useState<User[]>(props.data);
+    // const [isLoading, setLoading] = useState(false)
+
+    const { data, error, isLoading } = useSWR("https://belajar-firebase-ea75b.firebaseio.com/sales.json", (url) => fetch(url).then(res => res.json()))
 
     useEffect(() => {
+        if (data) {
+        setSales(normalizeData(data));
+      }
+    }, [data]);
+
+    /*useEffect(() => {
         setLoading(true)
         fetch('https://belajar-firebase-ea75b.firebaseio.com/sales.json')
             .then(res => res.json())
@@ -35,24 +57,38 @@ export default function LastSales() {
                 setLoading(false)
                 throw err
             })
-    }, [])
+    }, [])*/
 
-    if (isLoading) {
-        return <p>Loading...</p>
-    }
     
-    if (!data) {
-        return <p>Empty Data</p>
+    if (error) {
+        return <p>Failed to load data</p>
+    }
+
+    if (!sales && !data) {
+        return <p>Empty data</p>;
     }
 
     return (
       <ul>
-        {data.map((item) => (
+        {sales.map((item: User) => (
           <li key={item.id}>
-                <p>{item.username}</p>
-                <p>{item.volume}</p>
+            <p>{item.id}</p>
+            <p>{item.username}</p>
+            <p>{item.volume}</p>
           </li>
         ))}
       </ul>
     );
+}
+
+export async function getStaticProps() {
+    const response = await fetch("https://belajar-firebase-ea75b.firebaseio.com/sales.json")
+    const data: Response = await response.json();
+    const res = normalizeData(data)
+    
+    return {
+        props: {
+            data: res
+        }
+    }
 }
